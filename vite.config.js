10ebb3dbc95a9f78b44838ec112ad4e6a58d4fb5
RabiKhan68 <Rabibro063@gmail.com>
2025-10-05 00:58:33 +0500

    inital

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Sien Calender',
+        short_name: 'Calender',
+        description: 'Your personal calender app',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/calender-icon.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/calender-icon.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+});
